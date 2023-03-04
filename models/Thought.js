bae87b7583d41c1aa98d99a37a36fb95be071720
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-03-04 14:27:11 -0500

    filled out thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,48 @@
-const mongoose = require('mongoose');
+const { ObjectId } = require("bson");
+const mongoose = require("mongoose");
 
+const ReactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: ObjectId,
+    default: new ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: trusted,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const ThoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema],
+});
+
+ThoughtSchema.virtual("reactionCount").get(() => {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", ThoughtSchema);
+
+module.exports = Thought;
