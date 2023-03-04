008d0546b17cff38ee2f91603f98944626ddbe33
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-03-04 12:01:52 -0500

    filled out folders with placeholder files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const {User, Thought} = require('./models');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
